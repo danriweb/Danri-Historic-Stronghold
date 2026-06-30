c52bd1a94b5f9c852d41d02f73eacac7320f2cdc
DanriWeb <danri.web@gmail.com>
2026-06-30 13:40:49 +0500

    chore(files): remove unnecessary

--- 1.20.1-Forge-Stronghold/src/main/java/com/danriweb/historic_stronghold/HistoricStronghold.java
+++
@@ -1,8 +0,0 @@
-package com.danriweb.historic_stronghold;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(HistoricStronghold.MOD_ID)
-public class HistoricStronghold {
-  public static final String MOD_ID = "historic_stronghold";
-}
