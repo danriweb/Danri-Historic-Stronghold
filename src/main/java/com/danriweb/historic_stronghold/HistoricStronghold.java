c814db33e736f040c04bd68ffa1f5cbcc0f9ea3f
DanriWeb <danri.web@gmail.com>
2026-06-30 13:28:50 +0500

    feat(app): initialize version 1.20.1 Fabric

---
+++ src/main/java/com/danriweb/historic_stronghold/HistoricStronghold.java
@@ -0,0 +1,12 @@
+package com.danriweb.historic_stronghold;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HistoricStronghold implements ModInitializer {
+    public static final String MOD_ID = "historic_stronghold";
+
+    @Override
+    public void onInitialize() {
+        // Initializer
+    }
+}
