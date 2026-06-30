25581da6af57ee68069b9f031b043b1c90813613
DanriWeb <danri.web@gmail.com>
2026-06-30 10:53:41 +0500

    feat(app): initialize version 26.1.2 NeoForge

---
+++ src/main/java/com/danriweb/historic_stronghold/WardenMethodDumper.java
@@ -0,0 +1,11 @@
+package com.danriweb.historic_stronghold;
+
+import net.minecraft.world.entity.monster.warden.Warden;
+
+public class WardenMethodDumper {
+  public static void main(String[] args) {
+    for (java.lang.reflect.Method m : Warden.class.getDeclaredMethods()) {
+      System.out.println(m.getName() + " - " + m.getReturnType().getName());
+    }
+  }
+}
---
+++ src/main/java/com/danriweb/historic_stronghold/client/ClientSetup.java
@@ -0,0 +1,16 @@
+package com.danriweb.historic_stronghold.client;
+
+import com.danriweb.historic_stronghold.registry.ModEntityTypes;
+import net.minecraft.client.renderer.entity.WardenRenderer;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+
+public class ClientSetup {
+  public static void init(IEventBus modEventBus) {
+    modEventBus.addListener(ClientSetup::registerEntityRenderers);
+  }
+
+  private static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+    event.registerEntityRenderer(ModEntityTypes.STRONGHOLD_GUARDIAN_WARDEN.get(), WardenRenderer::new);
+  }
+}
